b64153062440d6a8c0c64c710ac5297db3076cf3
shantanuphatke <sphatkes@gmail.com>
2021-04-11 13:55:34 +0530

    updated client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,111 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
-  return <div className="App">asddas</div>;
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [listOfFriends, setListOfFriends] = useState([]);
+
+  const addFriend = () => {
+    Axios.post("https://mernstack-deployment.herokuapp.com/addfriend", {
+      name: name,
+      age: age,
+    }).then((response) => {
+      setListOfFriends([
+        ...listOfFriends,
+        { _id: response.data._id, name: name, age: age },
+      ]);
+    });
+  };
+
+  const updateFriend = (id) => {
+    const newAge = prompt("Enter new age: ");
+
+    Axios.put("https://mernstack-deployment.herokuapp.com/update", {
+      newAge: newAge,
+      id: id,
+    }).then(() => {
+      setListOfFriends(
+        listOfFriends.map((val) => {
+          return val._id == id ? { _id: id, name: val.name, age: newAge } : val;
+        })
+      );
+    });
+  };
+
+  const deleteFriend = (id) => {
+    Axios.delete(`https://mernstack-deployment.herokuapp.com/delete/${id}`).then(
+      () => {
+        setListOfFriends(
+          listOfFriends.filter((val) => {
+            return val._id != id;
+          })
+        );
+      }
+    );
+  };
+
+  useEffect(() => {
+    Axios.get("https://mernstack-deployment.herokuapp.com/read")
+      .then((response) => {
+        setListOfFriends(response.data);
+      })
+      .catch(() => {
+        console.log("ERR");
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="inputs">
+        <input
+          type="text"
+          placeholder="Friend name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Friend age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+
+        <button onClick={addFriend}>Add Friend</button>
+      </div>
+
+      <div className="listOfFriends">
+        {listOfFriends.map((val) => {
+          return (
+            <div className="friendContainer">
+              <div className="friend">
+                <h3>Name: {val.name}</h3>
+                <h3> Age: {val.age}</h3>
+              </div>
+              <button
+                onClick={() => {
+                  updateFriend(val._id);
+                }}
+              >
+                Update
+              </button>
+              <button
+                id="removeBtn"
+                onClick={() => {
+                  deleteFriend(val._id);
+                }}
+              >
+                X
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
